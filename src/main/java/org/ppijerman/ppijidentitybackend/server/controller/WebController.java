c558bf5bda5f12c73dae44c7d69d0ae537c86654
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2022-03-15 00:01:10 +0100

    added routes and mapping the request

--- src/main/java/org/ppijerman/ppijidentitybackend/server/controller/WebController.java
+++ src/main/java/org/ppijerman/ppijidentitybackend/server/controller/WebController.java
@@ -6,7 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
     @GetMapping("/")
-    String home() {
+    public String home() {
         return "index";
     }
+
+    @GetMapping("/login")
+    public String login() { return "login"; }
+
+    @GetMapping("/logout")
+    public String logout() { return "logout"; }
+
+    @GetMapping("/privacy-policy")
+    public String privacyPolicy() { return "privacyPolicy"; }
+
+    @GetMapping("/register")
+    public String register() { return "register"; }
 }
