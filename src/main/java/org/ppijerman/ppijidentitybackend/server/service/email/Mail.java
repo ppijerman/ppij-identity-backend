9b2d4767ef70753b2892517e4b6bc8ccaef1e304
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2022-04-06 11:56:30 +0200

    added email controller

---
+++ src/main/java/org/ppijerman/ppijidentitybackend/server/service/email/Mail.java
@@ -0,0 +1,4 @@
+package org.ppijerman.ppijidentitybackend.server.service.email;
+
+public class Mail {
+}
---
+++ src/main/java/org/ppijerman/ppijidentitybackend/server/service/email/NotificationRequest.java
@@ -0,0 +1,9 @@
+package org.ppijerman.ppijidentitybackend.server.service.email;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class NotificationRequest {
+
+    
+}
