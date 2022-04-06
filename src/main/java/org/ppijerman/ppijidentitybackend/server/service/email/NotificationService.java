37ba9a58fe6cdb5d6f1dc38fde3607e4e27d32bb
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2022-04-06 12:17:27 +0200

    added notificationservice for mail handling

--- src/main/java/org/ppijerman/ppijidentitybackend/server/service/email/NotificationService.java
+++ src/main/java/org/ppijerman/ppijidentitybackend/server/service/email/NotificationService.java
@@ -1,4 +1,24 @@
 package org.ppijerman.ppijidentitybackend.server.service.email;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
 public class NotificationService {
+
+    // Simple class which does the mail handling
+    private final JavaMailSender javaMailSender;
+
+    public NotificationService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void notifyUser(String email, String content) {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setFrom("admin@spring.io");        // static address
+        mail.setSubject("Email Confirmation");  // static subject
+        mail.setText(content);
+        mail.setTo(email);
+
+        this.javaMailSender.send(mail);
+    }
 }
