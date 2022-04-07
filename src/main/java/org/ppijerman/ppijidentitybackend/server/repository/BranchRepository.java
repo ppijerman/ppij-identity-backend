53d60fef9385577a1085146f764a94b670e8f758
rtharivandi <24.raihan@gmail.com>
2022-04-07 21:52:34 +0200

    Added all needed repositories

--- src/main/java/org/ppijerman/ppijidentitybackend/server/repository/BranchRepository.java
+++ src/main/java/org/ppijerman/ppijidentitybackend/server/repository/BranchRepository.java
@@ -1,13 +1,5 @@
 package org.ppijerman.ppijidentitybackend.server.repository;
 
-<<<<<<< HEAD
-import org.ppijerman.ppijidentitybackend.server.dto.Branch;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BranchRepository extends JpaRepository<Branch, Integer> {
-    
-}
-=======
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.ppijerman.ppijidentitybackend.server.dto.Branch;
 import java.util.UUID;
@@ -17,4 +9,3 @@ public interface BranchRepository extends JpaRepository<Branch, UUID> {
 }
     
 
->>>>>>> feature/JPA-class
