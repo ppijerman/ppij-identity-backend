05320377ba4b06b241cbb5dbc72c3bef8a76dca0
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2022-04-16 13:55:17 +0200

    remove scope part of tomcat to avoid error

--- src/main/java/org/ppijerman/ppijidentitybackend/server/service/registration/RegistrationService.java
+++ src/main/java/org/ppijerman/ppijidentitybackend/server/service/registration/RegistrationService.java
@@ -1,5 +1,8 @@
 package org.ppijerman.ppijidentitybackend.server.service.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
         return "works";
