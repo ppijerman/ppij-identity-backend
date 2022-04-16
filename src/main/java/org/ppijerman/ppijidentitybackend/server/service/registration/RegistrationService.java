1780ec60284e66c974e87be0d37b7e7c17e1510f
Adib Waluya <50482736+adibwaluya@users.noreply.github.com>
2022-04-16 13:09:30 +0200

    added initial setting for user registration before implementing spring security

---
+++ src/main/java/org/ppijerman/ppijidentitybackend/server/service/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package org.ppijerman.ppijidentitybackend.server.service.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return null;
+    }
+}
